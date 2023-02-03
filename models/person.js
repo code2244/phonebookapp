4d0b7089b306da4370d1c8dc5f03417d085a1a82
Ricardo Hernandez <rmhm.photography@gmail.com>
2023-02-03 14:35:16 -0400

    fixed mongodb connection.

--- models/person.js
+++ models/person.js
@@ -4,25 +4,25 @@ mongoose.set('strictQuery', false)
 const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+	.then(result => {
+		console.log('connected to MongoDB')
+	})
+	.catch((error) => {
+		console.log('error connecting to MongoDB:', error.message)
+	})
 
-// Phone Schema 
+// Phone Schema
 const phonebookSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+	name: String,
+	number: String,
 })
 
 phonebookSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	}
 })
 
 module.exports = mongoose.model('Person', phonebookSchema)
--- mongo.js
+++ mongo.js
@@ -1,14 +1,15 @@
+/* eslint-disable no-undef */
 const mongoose = require('mongoose')
 
 // Validation
 
 if (process.argv.length < 3) {
-  console.log('Password is missing or incorrect')
-  process.exit(1)
-} 
+	console.log('Password is missing or incorrect')
+	process.exit(1)
+}
 else if (process.argv.length === 4) {
-  console.log("Phone number is missing")
-  process.exit(1)
+	console.log('Phone number is missing')
+	process.exit(1)
 }
 
 // Connect to mongodb
@@ -20,30 +21,30 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 
 // Define data model
 const phonebookSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+	name: String,
+	number: String,
 })
 
 const Person = mongoose.model('Person', phonebookSchema)
 
 if (process.argv.length === 3) {
-  // List all people in phone book
-  Person.find({}).then(result => {
-    result.forEach(person => {
-      console.log(person)
-    })
-    mongoose.connection.close()
-  })
+	// List all people in phone book
+	Person.find({}).then(result => {
+		result.forEach(person => {
+			console.log(person)
+		})
+		mongoose.connection.close()
+	})
 }
 else if (process.argv.length === 5) {
-  // add new person to phone book
-  const person = new Person({
-    name: process.argv[3],
-    number: process.argv[4]
-  })
+	// add new person to phone book
+	const person = new Person({
+		name: process.argv[3],
+		number: process.argv[4]
+	})
 
-  person.save().then(response => {
-    console.log(`Added "${process.argv[3]}" with number "${process.argv[4]}" to phonebook`)
-    mongoose.connection.close()
-  })
+	person.save().then(response => {
+		console.log(`Added "${process.argv[3]}" with number "${process.argv[4]}" to phonebook`)
+		mongoose.connection.close()
+	})
 }
